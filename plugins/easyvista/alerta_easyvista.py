ce9b66b5c7292a8d929b8ea2a92c5d85ba1f6e97
ycyr <1794646+ycyr@users.noreply.github.com>
2019-03-13 16:32:17 -0400

    Cleaning code

--- plugins/easyvista/alerta_easyvista.py
+++ plugins/easyvista/alerta_easyvista.py
@@ -16,49 +16,62 @@ EASYVISTA_PASSWORD = os.environ.get('EASYVISTA_PASSWORD') or app.config['EASYVIS
 EASYVISTA_CATALOGID = os.environ.get('EASYVISTA_CATALOGID') or app.config['EASYVISTA_CATALOGID']
 EASYVISTA_CUSTOMERS = os.environ.get('EASYVISTA_CUSTOMERS') or app.config['EASYVISTA_CUSTOMERS']
 
-data = {"requests":[{"Catalog_Code": EASYVISTA_CATALOGID ,"Recipient.Last_name":"Test","description":"Alerte générer depuis Alerta"}]}
+data = {"requests":[{"Catalog_Code": EASYVISTA_CATALOGID ,"Recipient.Last_name":"Test","description":"Alerte généré depuis Alerta"}]}
 
 
 class EasyVistaAlert(PluginBase):
+
+    def create_ticket(self, alert):
+
+        r = requests.post(EASYVISTA_URL, auth=(EASYVISTA_USERNAME, EASYVISTA_PASSWORD), json=data)
+        LOG.info("Réponse HTTP: {} et Explication: {} ".format(r.status_code, r.reason))
+        alert.attributes['ITSM'] = re.findall("(INC\d+)", r.json()["HREF"])[0]
+        LOG.info("Le ticket {} a été crée dans EasyVista".format(re.findall("(INC\d+)", r.json()["HREF"])[0]))
+
+        return
+
+
+    def retreive_ticket_status(self, alert):
+
+        url = EASYVISTA_URL + '/' + str(alert.attributes['ITSM'])
+        r = requests.get(url, auth=(EASYVISTA_USERNAME, EASYVISTA_PASSWORD)  )
+        LOG.info("Réponse HTTP: {} et Explication: {} ".format(r.status_code, r.reason))
+        ticket_status = r.json()["STATUS"]["STATUS_FR"]
+
+        return ticket_status
+
 
     def pre_receive(self, alert):
 
         return alert
 
+
+
     def post_receive(self, alert):
 
         LOG.info("Enhancing alert ITSM ")
-
-
+        
+        
         if  alert.customer != None and alert.customer in EASYVISTA_CUSTOMERS:
+
             LOG.info("BON CUSTOMER")
             LOG.info("Nombre de duplicas: {}".format(alert.duplicate_count))
-            if  alert.duplicate_count > 0:
-                LOG.info("Alerte dupliquée: No Ticket actuel: {}".format(alert.attributes['ITSM']))
-                url = EASYVISTA_URL +'/' + str(alert.attributes['ITSM'])
-                r = requests.get(url, auth=(EASYVISTA_USERNAME, EASYVISTA_PASSWORD)  )
-                LOG.info(r.status_code)
-                LOG.info(r.reason)
-                ticket_status = r.json()["STATUS"]["STATUS_FR"]
-                LOG.info(ticket_status)
 
-                if r.json()["STATUS"]["STATUS_FR"] =="Clôturé":
+            if  alert.duplicate_count > 0:
+
+                LOG.info("Alerte dupliquée: No Ticket actuel: {}".format(alert.attributes['ITSM']))
+                LOG.info("Status du ticket {}: {}".format(alert.attributes['ITSM'], self.retreive_ticket_status(alert)))
+
+                if self.retreive_ticket_status(alert) =="Clôturé":
+
                     LOG.info("Ouverture d'un nouveau ticket EASYVISTA car le ticket {} a été clôturé".format(alert.attributes['ITSM']))
-                    r = requests.post( EASYVISTA_URL, auth=(EASYVISTA_USERNAME, EASYVISTA_PASSWORD), json=data)
-                    LOG.info(r.status_code)
-                    LOG.info(r.reason)
-                    LOG.info(r.json())
-                    alert.attributes['ITSM'] = re.findall("(INC\d+)", r.json()["HREF"])[0]
-                    LOG.info("Le ticket {} a été crée dans EasyVista".format(re.findall("(INC\d+)", r.json()["HREF"])[0]))
+
+                    self.create_ticket(alert)
             else:
+
                 LOG.info("Ouverture d'un nouveau ticket EASYVISTA")
 
-                r = requests.post( EASYVISTA_URL, auth=(EASYVISTA_USERNAME, EASYVISTA_PASSWORD), json=data)
-                LOG.info(r.status_code)
-                LOG.info(r.reason)
-                LOG.info(r.json())
-                alert.attributes['ITSM'] = re.findall("(INC\d+)", r.json()["HREF"])[0]
-                LOG.info("Le ticket {} a été crée dans EasyVista".format(re.findall("(INC\d+)", r.json()["HREF"])[0]))
+                self.create_ticket(alert)
 
         else:
             LOG.info("PAS BON CUSTOMER")
